8b22c62618a80c68ed8da16bc48132169b7228ba
DylanDeFrank <102638085+DylanDeFrank@users.noreply.github.com>
2025-12-13 22:06:26 -0800

    Fetch and parse data from a server
    Connected the fetch request to input value from
    the user with template literal and back ticks. I
    was using the encodeURIComponent incorrectly.
    
    Created parse and div functions that will drill
    into the object, retrieve needed info of weather,
    and eventually display it.

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 async function getTemp(x){
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(x)}?key=FGUH84J4RUTKF4JCPMQHZGSP2`)
     const data = await response.json();
-    console.log(data);
+    console.log(data)
+    return data
 }
 const form = document.getElementById("cityForm");
 const input = document.getElementById("cityInput");
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", async function(event) {
     event.preventDefault();
     console.log(input.value)
     const x = input.value    
     console.log(x)
     console.log(encodeURIComponent(x))
-    getTemp(x)
+    const getData = await getTemp(x)
+    console.log(getData)
+    parseData(getData.days)
+    createGrid()
 })
+
+function parseData(array){
+    for (let index = 0; index < 7; index++) {
+        const element = array[index];
+        console.log(element)
+    }
+}
+
+function createGrid(){
+    const  container = document.createElement("div")
+    container.classList.add("container")
+    const addTo = document.querySelector("body")
+    addTo.appendChild(container)
+
+}
+function sayHi(nme, hello){
+    greeting(nme)
+}
+
+function greeting(nme){
+    console.log(`hello! ${nme}`)
+}
